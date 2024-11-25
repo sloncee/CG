0c7be4536076da0b8844af1c7efa7d4807bfb9dd
sloncee <sofya.dorofeyeva@yandex.ru>
2024-11-25 08:27:45 +0300

    add Matrix2f and Matrix2fTest

---
+++ task3/src/main/java/ru/vsu/cs/dorofeyeva_s_v/math/matrices/Matrix2f.java
@@ -0,0 +1,87 @@
+package ru.vsu.cs.dorofeyeva_s_v.math.matrices;
+
+import lombok.Getter;
+import lombok.Setter;
+import ru.vsu.cs.dorofeyeva_s_v.math.vectors.Vector2f;
+import ru.vsu.cs.dorofeyeva_s_v.math.vectors.Vector3f;
+
+@Getter
+@Setter
+public class Matrix2f {
+    private Vector2f v1;
+    private Vector2f v2;
+
+    public Matrix2f() {
+        this.v1 = new Vector2f();
+        this.v2 = new Vector2f();
+    }
+    public Matrix2f(Vector2f v1, Vector2f v2) {
+        this.v1 = v1;
+        this.v2 = v2;
+    }
+
+    public static Matrix2f unitMatrix2f() {
+        return new Matrix2f(new Vector2f(1, 0),
+                new Vector2f(0, 1));
+    }
+
+    public static Matrix2f zeroMatrix2f() {
+        return new Matrix2f(new Vector2f(0, 0),
+                new Vector2f(0, 0));
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || this.getClass() != o.getClass()) {
+            return false;
+        }
+        Matrix2f matrix2f = (Matrix2f) o;
+        return this.v1.equals(matrix2f.getV1())
+                && this.v2.equals(matrix2f.getV2());
+    }
+
+    public Matrix2f add(Matrix2f matrix2f) {
+        this.v1.add(matrix2f.getV1());
+        this.v2.add(matrix2f.getV2());
+        return this;
+    }
+
+    public Matrix2f subtract(Matrix2f matrix2f) {
+        this.v1.subtract(matrix2f.getV1());
+        this.v2.subtract(matrix2f.getV2());
+        return this;
+    }
+
+    public Vector2f multiplyByVector(Vector2f vector2f) {
+        Vector2f result = new Vector2f(0, 0);
+        result.setX(this.v1.scalarProduct(vector2f));
+        result.setY(this.v2.scalarProduct(vector2f));
+        return result;
+    }
+
+    public Matrix2f multiplyByMatrix(Matrix2f matrix2f) {
+        Vector2f v1Result = new Vector2f(0, 0);
+        v1Result.setX(this.v1.scalarProduct(new Vector2f(matrix2f.getV1().getX(), matrix2f.getV2().getX())));
+        v1Result.setY(this.v1.scalarProduct(new Vector2f(matrix2f.getV1().getY(), matrix2f.getV2().getY())));
+
+        Vector2f v2Result = new Vector2f(0, 0);
+        v2Result.setX(this.v2.scalarProduct(new Vector2f(matrix2f.getV1().getX(), matrix2f.getV2().getX())));
+        v2Result.setY(this.v2.scalarProduct(new Vector2f(matrix2f.getV1().getY(), matrix2f.getV2().getY())));
+
+        return new Matrix2f(v1Result, v2Result);
+    }
+
+    public Matrix2f transpose() {
+        Vector2f v1Result = new Vector2f(0, 0);
+        v1Result.setX(this.v1.getX());
+        v1Result.setY(this.v2.getX());
+
+        Vector2f v2Result = new Vector2f(0, 0);
+        v2Result.setX(this.v1.getY());
+        v2Result.setY(this.v2.getY());
+
+        return new Matrix2f(v1Result, v2Result);
+    }
+}
---
+++ task3/src/test/java/ru/vsu/cs/dorofeyeva_s_v/math/vectors/Matrix2fTest.java
@@ -0,0 +1,48 @@
+package ru.vsu.cs.dorofeyeva_s_v.math.vectors;
+
+import org.junit.jupiter.api.Test;
+import ru.vsu.cs.dorofeyeva_s_v.math.matrices.Matrix2f;
+
+import static junit.framework.Assert.assertEquals;
+
+public class Matrix2fTest {
+    @Test
+    public void testAddition() {
+        Matrix2f m1 = new Matrix2f(new Vector2f(1, 2), new Vector2f(3, 4));
+        Matrix2f m2 = new Matrix2f(new Vector2f(5, 6), new Vector2f(7, 8));
+        Matrix2f result = m1.add(m2);
+        assertEquals(new Matrix2f(new Vector2f(6, 8), new Vector2f(10, 12)), result);
+    }
+
+    @Test
+    public void testSubtraction() {
+        Matrix2f m1 = new Matrix2f(new Vector2f(1, 2), new Vector2f(3, 4));
+        Matrix2f m2 = new Matrix2f(new Vector2f(5, 6), new Vector2f(7, 8));
+        Matrix2f result = m1.subtract(m2);
+        assertEquals(new Matrix2f(new Vector2f(-4, -4), new Vector2f(-4, -4)), result);
+    }
+
+    @Test
+    public void testMultiplicationByVector() {
+        Matrix2f m1 = new Matrix2f(new Vector2f(1, 2), new Vector2f(3, 4));
+        Vector2f v1 = new Vector2f(9, 10);
+        Vector2f result = m1.multiplyByVector(v1);
+        assertEquals(new Vector2f(29, 67), result);
+    }
+
+    @Test
+    public void testMultiplicationByMatrix() {
+        Matrix2f m1 = new Matrix2f(new Vector2f(1, 2), new Vector2f(3, 4));
+        Matrix2f m2 = new Matrix2f(new Vector2f(5, 6), new Vector2f(7, 8));
+        Matrix2f result = m1.multiplyByMatrix(m2);
+        assertEquals(new Matrix2f(new Vector2f(19, 22), new Vector2f(43, 50)), result);
+    }
+
+    @Test
+    public void testTransposition() {
+        Matrix2f m1 = new Matrix2f(new Vector2f(1, 2), new Vector2f(3, 4));
+        Matrix2f result = m1.transpose();
+        assertEquals(new Matrix2f(new Vector2f(1, 3), new Vector2f(2, 4)), result);
+    }
+}
+
